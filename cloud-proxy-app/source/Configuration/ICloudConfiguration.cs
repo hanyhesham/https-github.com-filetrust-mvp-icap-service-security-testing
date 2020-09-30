f6a363e9e1dec51095f9a21231428aa2dbd15e6b
PEpetra <61695423+PEpetra@users.noreply.github.com>
2020-09-30 16:38:13 +0100

    Testing ICAp service

---
+++ cloud-proxy-app/source/Configuration/ICloudConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Glasswall.IcapServer.CloudProxyApp.Configuration
+{
+    public interface ICloudConfiguration
+    {
+        public string FileProcessingStorageConnectionString { get; set; }
+        public string FileProcessingStorageOriginalStoreName { get; set; }
+        public string TransactionOutcomeQueueConnectionString { get; }
+        public string TransactionOutcomeQueueName { get; }
+    }
+}
---
+++ cloud-proxy-app/source/Setup/ServiceCollectionExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using Glasswall.IcapServer.CloudProxyApp.Configuration;
+using Glasswall.IcapServer.CloudProxyApp.QueueAccess;
+using Glasswall.IcapServer.CloudProxyApp.StorageAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Glasswall.IcapServer.CloudProxyApp.Setup
+{
+    public static class ServiceCollectionExtensionMethods
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddLogging(configure => configure.AddConsole());
+
+            serviceCollection.AddSingleton<CloudProxyApplication>();
+            serviceCollection.AddTransient<IUploader, StorageUploader>();
+            serviceCollection.AddTransient<IServiceQueueClient, ServiceBusQueueClient>();
+
+            var appConfig = new CloudProxyApplicationConfiguration();
+            configuration.Bind(appConfig);
+            serviceCollection.AddSingleton<IAppConfiguration>(appConfig);
+
+            var cloudConfig = new CloudProxyCloudConfiguration();
+            configuration.Bind(cloudConfig);
+            serviceCollection.AddSingleton<ICloudConfiguration>(cloudConfig);
+
+            return serviceCollection;
+        }
+    }
+}
